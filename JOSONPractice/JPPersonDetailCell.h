b3bd69e3044414f7a10fcd7d0a5b078a1af42b64
tanduynguyen <tanduy.nguyen@2359media.us>
2013-01-18 14:04:31 +0700

    implement personal detail view

---
+++ JOSONPractice/JPPersonDetailCell.h
@@ -0,0 +1,13 @@
+//
+//  JPPersonDetailCell.h
+//  JOSONPractice
+//
+//  Created by viet on 1/18/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPPersonDetailCell : UITableViewCell
+
+@end
---
+++ JOSONPractice/JPPersonDetailCell.m
@@ -0,0 +1,29 @@
+//
+//  JPPersonDetailCell.m
+//  JOSONPractice
+//
+//  Created by viet on 1/18/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import "JPPersonDetailCell.h"
+
+@implementation JPPersonDetailCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
